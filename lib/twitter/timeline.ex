0ee768b8f8ee07f61b46d08693a8e016cf7da7e0
Nicolas Cavigneaux <nico@bounga.org>
2020-04-30 14:49:52 +0200

    Add distributed likes and reposts count atomic update

--- lib/twitter/timeline.ex
+++ lib/twitter/timeline.ex
@@ -104,6 +104,22 @@ defmodule Twitter.Timeline do
     Post.changeset(post, attrs)
   end
 
+  def increment_likes(%Post{id: id}) do
+    {1, [post]} =
+      from(p in Post, where: p.id == ^id, select: p)
+      |> Repo.update_all(inc: [likes_count: 1])
+
+    broadcast({:ok, post}, :post_updated)
+  end
+
+  def increment_reposts(%Post{id: id}) do
+    {1, [post]} =
+      from(p in Post, where: p.id == ^id, select: p)
+      |> Repo.update_all(inc: [reposts_count: 1])
+
+    broadcast({:ok, post}, :post_updated)
+  end
+
   def subscribe() do
     Phoenix.PubSub.subscribe(Twitter.PubSub, "posts")
   end
--- lib/twitter_web/live/post_live/post_component.ex
+++ lib/twitter_web/live/post_live/post_component.ex
@@ -1,3 +1,15 @@
 defmodule TwitterWeb.PostLive.PostComponent do
   use TwitterWeb, :live_component
+
+  def handle_event("like", _payload, socket) do
+    Twitter.Timeline.increment_likes(socket.assigns.post)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("repost", _payload, socket) do
+    Twitter.Timeline.increment_reposts(socket.assigns.post)
+
+    {:noreply, socket}
+  end
 end
